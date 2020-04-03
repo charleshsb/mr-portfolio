d9c0aed191e6987b86e3237ad9926921d7526aab
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2020-04-03 18:03:36 +0200

    swiper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,22 @@
 import "bootstrap";
+import "animate.css/animate.min.css";
+import Swiper from 'swiper';
 
 var mixitup = require('mixitup');
 if (document.querySelector('.cards')) {
   var mixer = mixitup('.cards');
 }
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  loop: true,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    hide: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
